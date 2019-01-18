6fc243c19be35a2a2a98cce6b53beb0eb3616c71
Karl Bloedorn <karl@bloedorn.us>
2019-01-17 22:14:04 -0600

    Added docs content

---
+++ docs/precache-manifest.6e6ba8457fff992ea2e867df9f863331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "577e93fde391bc4e25a4",
+    "url": "/static/css/main.82991f14.chunk.css"
+  },
+  {
+    "revision": "577e93fde391bc4e25a4",
+    "url": "/static/js/main.577e93fd.chunk.js"
+  },
+  {
+    "revision": "11844ada171cdd05fcea",
+    "url": "/static/js/1.11844ada.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7f365ebf49143746395688eae85567a6",
+    "url": "/index.html"
+  }
+];
